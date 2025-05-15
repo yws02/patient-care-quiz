9f6382ff585ed3a73311a244217b675ecee6f327
yws888 <61619668+yws888@users.noreply.github.com>
2025-05-16 00:28:23 +0800

    Revert "test deploy"
    This reverts commit 336f5ec57937dc4c4bf088689967cc105e2694a1.

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-// const API_URL = 'http://localhost:8888/api/quiz' ;
-
 const API_URL = process.env.REACT_APP_API_URL || 'https://patient-care-quiz-backend.onrender.com/api/quiz';
 
 export const submitPatientData = async (patientData) => {
